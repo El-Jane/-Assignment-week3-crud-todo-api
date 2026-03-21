855dd5b73efee76b04aea6c165df0c0ce93af79c
Nuella <ellaedu04@gmail.com>
2026-03-21 11:02:21 +0000

    Week 4

--- app.js
+++ app.js
@@ -2,49 +2,88 @@ require('dotenv').config();
 
 const express = require ("express");
 const cors = require('cors'); //cors
+const logRequest = require('./middlewares/logger.js');
+const validator = require('./middlewares/validator.js');
+const errorhandler = require('./middlewares/errorHandler.js');
 const app = express ();
 
 //body parsing middleware
-app.use[express.json()];
-app.use[cors('*')];
+app.use(express.json());
+app.use(cors('*'));
+app.use(logRequest);
 
 let todos = [
-{ "id": 1, task: 'Learn Node.js', completed: false},
+ {"id": 1, task: 'Learn Node.js', completed: false},
 { "id": 2, task: 'Build CRUD API', completed: false},
 { "id": 3, task: 'Build API', completed: true},
 { "id": 4, task: 'Learning new', completed: true},
 ];
 
-app.get ('/todos', (req, res) => {
-    res.status(200).json(todos); // send array as JSON
+app.get('/todos/completed', (req, res, next) => {
+try {
+    const completed = todos.filter((t) => t.completed);
+res.json(completed); //Custom Read!
+} catch (error) {
+    next(error);
+}
 });
 
-app.post('/todos', (req, res) => {
-const newTodo = { id: todos.length + 1, ...req.body }; //Auto.ID
+app.get ('/todos/:id', (req, res, next) => {
+try {
+    const id = parseInt(req.params.id);
+    if(isNaN(id)) {
+        throw new Error("invalid ID")
+}
+const todo = todos.find((t) => t.id === id);
+
+if (!todo) {
+    return res.status(404).json({ message: 'Todo not found' });
+}
+        res.status(200).json(todo); // send array as JSON
+} catch (error) {
+    next(error);
+}
+});
+
+app.post('/todos', validator.validatePostTodo, (req, res, next) => {
+try {
+    const newTodo = { id: todos.length + 1, ...req.body }; //Auto.ID
 todos.push(newTodo);
 res.status(201).json(newTodo); //Echo back
+} catch (error) {
+    next(error);
+}
 });
 
 // PATCH Update ¬ Partial
-app.patch('/todos/:id', (req, res) => {
-    const todo =todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
+app.patch('/todos/:id', validator.validatePatchTodo, (req, res, next) => {
+    try {
+        const todo =todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
     if (!todo) return res.status(404).json({ message: 'Todo not found'});
     Object.assign(todo, req.body); //Merge: e.g., {completed: true}
     res.status(200).json(todo);
+    } catch (error) {
+        next(error);
+    }
 });
 
 // DELETE Remove
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', (req, res, next) => {
+try {
     const id = parseInt(req.params.id);
     const initialLenght = todos.length;
     todos = todos.filter((t) => t.id !== id); //Array.filter() - non-destructive
     if (todos.lenght === initialLenght) return res.status(404).json({ error: "Not found" });
     res.status(204).send (); //Silent success
+
+} catch (error) {
+next(error);    
+}    
 });
 
-app.use((err, req, res, next) => {
-    res.status(500).json({ error: 'Server error!' });
-})
+
+app.use(errorhandler);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
